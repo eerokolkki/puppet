fe346b33e40357d19da31091246195896e713fa5
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2016-11-27 14:27:16 +0200

    Update init.pp

--- modules/lampstack/manifests/init.pp
+++ modules/lampstack/manifests/init.pp
@@ -16,13 +16,15 @@ require => Package["apache2"],
 file { "/var/www/html/index.html":
 ensure => "absent",
 require => Package["apache2"],
+}
 file { "/etc/apache2/sites-available/${title}.com.conf":
 content => template("lampstack/vhost.conf.erb"),
+require => Package["apache2"],
 }
-
 file { "/etc/apache2/sites-enabled/${title}.com.conf":
 ensure => "link",
 target => "../sites-available/${title}.com.conf",
+require => Package["apache2"],
 }
 service { "apache2":
 ensure => "running",
