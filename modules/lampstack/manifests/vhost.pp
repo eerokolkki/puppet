7c8100149fdf25e44049ce8af29871907e7062e2
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2016-11-28 12:28:19 +0200

    Update vhost.pp

--- modules/lampstack/manifests/vhost.pp
+++ modules/lampstack/manifests/vhost.pp
@@ -2,10 +2,12 @@ define lampstack::vhost () {
 	
 	file { "/etc/apache2/sites-available/${title}.com.conf":
 		content => template("lampstack/vhost.conf.erb"),
+		require => Package["apache2"],
         }
 
 	file { "/etc/apache2/sites-enabled/${title}.com.conf":
 		ensure => "link",
 		target => "../sites-available/${title}.com.conf",
+		require => Package["apache2"],
 	}
 }
