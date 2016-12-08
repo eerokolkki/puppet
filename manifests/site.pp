01bfece10d31f1dd241ed9818c11553c1a803b52
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2016-12-08 23:16:29 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 class { "lampstack":}
-Lampstack::Vhost {
-notify => Service["apache2"],
-}
-lampstack::vhost {"eerokolkki":}
-lampstack::vhost {"pulla":}
+        Lampstack::Vhost {
+            notify => Service["apache2"],
+            }
+            lampstack::vhost {"eerokolkki":}
+            lampstack::vhost {"pulla":}
 
-class { "mysql::server":
- root_password => "K4l4kukk0",
-}
-class { "ssh1":}
-class { "tausta":}
+        class { "mysql::server":
+            root_password => "K4l4kukk0",
+        }
+        class { "ssh1":}
+        class { "tausta":}
