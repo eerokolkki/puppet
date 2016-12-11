7112fa069f1b2662334d811c2507e96608d82ff9
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2016-12-11 15:55:04 +0200

    Update init.pp

--- modules/tausta/manifests/init.pp
+++ modules/tausta/manifests/init.pp
@@ -1,14 +1,14 @@
 class tausta {
         file {"/usr/share/xfce4/backdrops/eero.jpeg":
-              source => "puppet:///modules/tausta/eero.jpeg",
+                source => "puppet:///modules/tausta/eero.jpeg",
         }
         file {"/usr/share/xfce4/backdrops/xubuntu-wallpaper.png":
-              ensure => "link",
-              target => "/usr/share/xfce4/backdrops/eero.jpeg",
+                ensure => "link",
+                target => "/usr/share/xfce4/backdrops/eero.jpeg",
         }
         service {"lightdm":
-        ensure => "running",
-        enable => "true",
-        provider => "systemd",
+                ensure => "running",
+                enable => "true",
+                provider => "systemd",
     }
 }
